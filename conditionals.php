309d3058b4f64d54282e55cb105f653db16efb5a
PGShanky <pgshanky405@gmail.com>
2022-05-04 11:04:17 +0300

    origin master

--- conditionals.php
+++ conditionals.php
@@ -1,8 +1,12 @@
 <?php
-$t = date("0");
-if ($t < "20"){
+$t = date("25");
+if ($t < "10"){
     echo "It's at night";
-} else if (){
-    echo ("It's morning");
+}
+else if ($t < "20"){
+    echo "Have a good evening";
+}
+else {
+    echo "Have a good night";
 }
 ?>
---
+++ loops.php
@@ -0,0 +1,20 @@
+<?php
+//DO LOOP
+//$y = "20";
+//while($y<"70"){
+//    echo "The nice number is : $y <br>";
+//    $y++;
+//}
+
+//DO WHILE LOOP
+//$y = "3";
+//do {
+//    echo "The number is: $y <br>";
+//    $y++;
+//} while($y <= "5");
+
+//FOR LOOP
+for ($x="10"; $x<="100"; $x++){
+    echo "The number is: $x <br>";
+}
+?>
